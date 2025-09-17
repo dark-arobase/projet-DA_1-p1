18bc5e25d33879c317037dbfce4409b9135863f9
Tahar Tiba <e6304876@cmaisonneuve.qc.ca>
2025-09-17 15:44:42 -0400

    Améliorer l'inscription

--- idée.py
+++ idée.py
@@ -15,6 +15,29 @@ button_font = ("Arial", 10, "bold")
 box_title_font = ("Arial", 12, "bold")
 box_text_font = ("Arial", 10)
 
+
+def ouvrir_inscription():
+    root.withdraw()  # Cache la fenêtre principale, mais ne la détruit pas
+
+    # Création de la nouvelle fenêtre
+    root2 = tk.Toplevel()
+    root2.title("INSCRIPTION")
+    root2.geometry("800x600")
+    root2.configure(bg="white")
+
+    label = tk.Label(root2, text="PAGE INSCRIPTION", font=header_font, bg="white")
+    label.pack(pady=40)
+
+    commentaire = tk.Entry(root2)
+    commentaire.pack(pady=40)
+
+    def revenir_accueil():
+        root2.destroy()     # Ferme la fenêtre d'inscription
+        root.deiconify()    # Ré-affiche la fenêtre principale
+
+    bt_retour = tk.Button(root2, text="RETOUR", command=revenir_accueil, bg="black", fg="white", font=button_font)
+    bt_retour.pack(pady=20)
+
 # ----------------------
 # En-tête (YUPPIQUIZ)
 # ----------------------
@@ -24,7 +47,7 @@ header.place(relx=0.5, rely=0.05, anchor="n")
 # ----------------------
 # Boutons haut : Inscription et Connexion
 # ----------------------
-btn_inscription = tk.Button(root, text="INSCRIPTION", bg="black", fg="white", font=button_font, padx=10, pady=5)
+btn_inscription = tk.Button(root, text="INSCRIPTION", bg="black", fg="white", font=button_font, padx=10, pady=5, command=ouvrir_inscription)
 btn_inscription.place(x=20, y=20)
 
 btn_connexion = tk.Button(root, text="SE CONNECTER", bg="black", fg="white", font=button_font, padx=10, pady=5)
