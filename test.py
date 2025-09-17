43436bc0bb95d3d7c95d343cb942a31e0616b29f
Michael <e6291960@cmaisonneuve.qc.ca>
2025-09-17 16:31:51 -0400

    yo

--- test.py
+++ test.py
@@ -4,8 +4,16 @@ import re
 root = tk.Tk()
 root.title("Menu principal")
 root.geometry("400x400")
-#root.attributes('-fullscreen', True)
-#root.configure(bg="#a2d5f2")
+
+header_font = ("Arial", 36, "bold")
+
+label = tk.Label(root, text = "Veuillez choisir un quiz ", font= header_font, background="black", fg="white",  )
+label.pack() 
+
+
+
+
+
 
 
 
